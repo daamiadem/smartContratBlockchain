f7525a56f9748722a1457eab8b67c34cffa8e4ce
DAAMI Adem <adem.daami@esprit.tn>
2023-03-31 14:22:33 +0100

    'smartcontrat'

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/M6AUC6iOmIVDFRmGWQW5cmm0niVJIQaS',
+      accounts: ['64f864b98b11ec14829251f058c6108e6549f69807a08b991d795ce381bf7a85'],
+
+      
+    },
+  },
+};
